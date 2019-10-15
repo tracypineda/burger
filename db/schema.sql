281ff0d7b07aea253b070da1d7a1125024804b10
Tracy Pineda <tracypineda@gmail.com>
2019-10-15 13:01:16 -0600

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE DATABASE burger_db;
+
+CREATE TABLE burgers (
+id: INT AUTO_INCREMENT NOT NULL,
+burger_name: VARCHAR(200) NOT NULL,
+devoured: BOOLEAN DEFAULT false NOT NULL
+primary key(id)
+);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("veggie burger", "false"), ("bacon cheddar", "false"), ("buffalo burger", "false");
